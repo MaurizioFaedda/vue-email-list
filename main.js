454009c5b9353fe3b18bc85b43712b74b7eb78a7
Maurizio <maurizio.faedda1@gmail.com>
2020-11-21 03:54:23 +0100

    name, mail, phone // mostra o nascondi valori

--- main.js
+++ main.js
@@ -2,31 +2,62 @@ var app = new Vue ({
     el: '#root',
 
     data: {
-        a: []
+        contacts: [],
     },
 
     methods: {
 
+        // isTrue(a, i){
+        //
+        //     if(this.contacts[i].a.success == false){
+        //     this.contacts[i].a.success = true
+        //     } else {
+        //     this.contacts[i].a.success = false
+        //     }
+        // }
+
+        isTrue_mail(i){
+
+            if(this.contacts[i].mail.success == false){
+            this.contacts[i].mail.success = true
+            } else {
+            this.contacts[i].mail.success = false
+            }
+        },
+
+        isTrue_phone(i){
+
+            if(this.contacts[i].phone.success == false){
+            this.contacts[i].phone.success = true
+            } else {
+            this.contacts[i].phone.success = false
+            }
+        }
     },
 
     mounted() {
        for (var i = 0; i < 10 ; i++) {
 
             let self = this;
-          let m = axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                .then(function(result) {
-                   let my_email = result.data.response;
+                   let my_email = result.data;
 
                    axios.get('https://flynn.boolean.careers/exercises/api/random/name')
-                   .then(function(res){
-                       let my_name = res.data.response;
-                       self.a.push({
-                           mail: my_email,
-                           name: my_name
+                       .then(function(result){
+                           let my_name = result.data;
+                           axios.get('https://flynn.boolean.careers/exercises/api/random/phone')
+                                .then(function(result) {
+                                    let my_phone = result.data;
+                                    self.contacts.push({
+                                        mail: my_email,
+                                        name: my_name,
+                                        phone: my_phone
+                                })
+                           })
+                           // console.log(m);
+                           // console.log(my_name);
                        })
-                       // console.log(m);
-                       // console.log(my_name);
-                   })
                });
 
 
